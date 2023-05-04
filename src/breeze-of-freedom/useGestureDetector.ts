9b8ae764ebbd79fd9e371c05b962f026239a9cec
wayne liberty <wayneliberty@waynedeMacBook-Air.local>
2023-05-04 23:22:25 +0800

    init

---
+++ src/breeze-of-freedom/useGestureDetector.ts
@@ -0,0 +1,3 @@
+import { HandLandmarker, FilesetResolver } from '@mediapipe/tasks-vision';
+
+export function useGestureDetector(config: { canvasElement: HTMLCanvasElement; videoElement: HTMLVideoElement }) {}
