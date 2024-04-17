076bfdbc14d51a94f7478ee87fe8af7676423e33
wayne.liberty <z.h.qing.t@gmail.com>
2024-04-17 18:19:18 +0800

    change boid

--- src/projects/GenerativeArtLearning/sketches/flocking.ts
+++ src/projects/GenerativeArtLearning/sketches/flocking.ts
@@ -39,7 +39,9 @@ export function flocking(p: p5) {
     p.rotate(b.velocity.heading());
     p.fill(255, 255, 255, 100);
     p.stroke(b.color.x, b.color.y, b.color.z, 255);
-    p.line(0, 0, 5, 0);
+    // p.line(0, 0, 5, 0);
+    // p.square(0, 0, 5)
+    p.ellipse(0, 0, 10, 3)
     p.pop();
   }
 
@@ -64,7 +66,7 @@ export function flocking(p: p5) {
   }
 
   function drawFrame() {
-    p.background(0, 0, 0, 10)
+    p.background(0, 0, 0, 5)
     boids.forEach(boid => {
       boid.run(boids);
     });
