071c7df95311b9e11739339b0a2660113a7d223b
wayne.liberty <z.h.qing.t@gmail.com>
2024-04-22 20:35:08 +0800

    save chagnes

--- src/projects/GenerativeArtLearning/sketches/circlePacking.ts
+++ src/projects/GenerativeArtLearning/sketches/circlePacking.ts
@@ -1,6 +1,20 @@
 import p5 from "p5";
 
-export function name(p: p5) {
+class Circle {
+  x: number;
+  y: number;
+  r: number;
+  constructor(x: number, y: number, r: number) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  }
+  draw(p: p5) {
+    p.ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+}
+
+export function circlePacking(p: p5) {
   const canvasWidth = 800;
   const canvasHeight = 800;
 
